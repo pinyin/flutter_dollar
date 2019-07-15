d58139421670064d132802ae94ca6235829af5be
pinyin <bob432091@gmail.com>
2019-07-15 10:59:14 +0800

    $State

--- lib/src/contexts.dart
+++ lib/src/contexts.dart
@@ -1,81 +1,45 @@
 import 'package:dollar/dollar.dart';
 import 'package:flutter/widgets.dart';
 
-mixin $StatelessWidget on StatelessWidget {
+mixin $State<T extends StatefulWidget> on State<T> {
   @override
-  StatelessElement createElement() {
-    // TODO make this composable
-    return _$StatelessElementProxy(this);
-  }
-}
-
-mixin $StatefulWidget on StatefulWidget {
-  @override
-  StatefulElement createElement() {
-    // TODO make this composable
-    return _$StatefulElementProxy(this);
-  }
-}
-
-class $Subtree extends StatelessWidget with $StatelessWidget {
-  final Function(BuildContext context) builder;
-
-  const $Subtree({Key key, this.builder}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return builder(context);
-  }
-}
-
-class _$StatefulElementProxy extends StatefulElement with _$ElementContext {
-  _$StatefulElementProxy($StatefulWidget widget) : super(widget);
-
-  Widget build() => $unbind(() => buildInContext(super.build));
-}
-
-class _$StatelessElementProxy extends StatelessElement with _$ElementContext {
-  _$StatelessElementProxy($StatelessWidget widget) : super(widget);
-
-  @override
-  Widget build() => $unbind(() => buildInContext(super.build));
-}
-
-mixin _$ElementContext on ComponentElement {
-  @override
-  void mount(Element parent, newSlot) {
+  void initState() {
+    super.initState();
     _createContext();
-    super.mount(parent, newSlot);
   }
 
   @override
   void reassemble() {
     super.reassemble();
-    _listeners.trigger($End());
-    try {
-      build();
-    } catch (e) {
-      _createContext();
-    }
+    _listeners.trigger($ContextTerminated());
+    _createContext();
   }
 
   _createContext() {
     _listeners = $Listeners();
-    buildInContext = $bind(
-      (func) => func(),
-      $combineHandlers([
-        $onUpdateVar((_) => markNeedsBuild()),
-        $listenAt(_listeners),
-      ]),
-    );
+    $isolate(() {
+      _build = $bind(
+        $build,
+        $combineHandlers([
+          $onVarUpdated((_) {
+            setState(() {});
+          }),
+          $onListened(_listeners),
+        ]),
+      );
+    });
   }
 
   @override
-  void unmount() {
-    _listeners.trigger($End());
-    super.unmount();
+  void dispose() {
+    _listeners.trigger($ContextTerminated());
+    super.dispose();
   }
 
   $Listeners _listeners;
-  Widget Function(Widget Function()) buildInContext;
+  Widget build(BuildContext context) => _build(context);
+
+  Widget $build(BuildContext context);
+
+  Widget Function(BuildContext context) _build;
 }
--- test/flutter_dollar_test.dart
+++ test/flutter_dollar_test.dart
@@ -5,33 +5,68 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   group('contexts', () {
     group('StatefulWidget', () {
-      runTest((builder, key) => Stateful(builder: builder, key: key));
-    });
-    group('StatelessWidget', () {
-      runTest((builder, key) => Stateless(builder: builder, key: key));
-    });
-    group('Subtree', () {
-      runTest((builder, key) => $Subtree(builder: builder, key: key));
-    });
-  });
+      testWidgets('should support dollar functions in build()', (tester) async {
+        var buildCount = 0;
+        var endCount = 0;
+        $Var<int> text;
+        await tester.pumpWidget(Stateful(builder: (context) {
+          buildCount++;
+          text = $var(() => 1);
 
-  group('extensions', () {
-    group('memo', () {
-      test('should return previous value iff deps are shallowly equal', () {
-        var runCount = 0;
-        final func = $bind((Iterable deps) {
-          return $memo(() => ++runCount, deps);
-        }, $emptyHandler);
-        expect(func([1, 2]), 1);
-        expect(func([1, 2]), 1);
-        expect(func([1, 2, 3]), 2);
-        expect(func([1, 2]), 3);
+          $fork(() {
+            return () => endCount++;
+          });
+          return Text(text.value.toString(), textDirection: TextDirection.ltr);
+        }));
+        expect(find.text('1'), findsOneWidget);
+        text.value++;
+        await tester.pump();
+        expect(find.text('2'), findsOneWidget);
+        text.value++;
+        await tester.pump();
+        expect(find.text('3'), findsOneWidget);
+        expect(buildCount, 3);
+        expect(endCount, 2);
+        await tester.pumpWidget(Container());
+        expect(endCount, 3);
+      });
+
+      testWidgets('should not interfere with inner components', (tester) async {
+        $Var<bool> buildInner;
+        await tester.pumpWidget(Stateful(builder: (context) {
+          buildInner = $var(() => true);
+          if (buildInner.value) {
+            return Stateful(
+              builder: (_) {
+                final cursor = $var(() => 'inner');
+                return Text(cursor.value, textDirection: TextDirection.ltr);
+              },
+              key: Key('inner'),
+            );
+          } else {
+            return Stateful(
+              builder: (_) {
+                $var(() => 1);
+                final cursor = $var(() => 'outer');
+                return Text(cursor.value, textDirection: TextDirection.ltr);
+              },
+              key: Key('outer'),
+            );
+          }
+        }));
+        expect(find.text('inner'), findsOneWidget);
+        buildInner.value = false;
+        await tester.pump();
+        expect(find.text('outer'), findsOneWidget);
+        buildInner.value = true;
+        await tester.pump();
+        expect(find.text('inner'), findsOneWidget);
       });
     });
   });
 }
 
-class Stateful extends StatefulWidget with $StatefulWidget {
+class Stateful extends StatefulWidget {
   final Widget Function(BuildContext context) builder;
 
   const Stateful({Key key, this.builder}) : super(key: key);
@@ -40,74 +75,9 @@ class Stateful extends StatefulWidget with $StatefulWidget {
   _StatefulState createState() => _StatefulState();
 }
 
-class _StatefulState extends State<Stateful> {
+class _StatefulState extends State<Stateful> with $State {
   @override
-  Widget build(BuildContext context) {
+  Widget $build(BuildContext context) {
     return widget.builder(context);
   }
-}
-
-class Stateless extends StatelessWidget with $StatelessWidget {
-  final Widget Function(BuildContext context) builder;
-
-  const Stateless({Key key, this.builder}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return builder(context);
-  }
-}
-
-void runTest(Widget widget(Widget builder(BuildContext context), Key key)) {
-  testWidgets('should support dollar functions in build()', (tester) async {
-    var buildCount = 0;
-    var endCount = 0;
-    $Var<int> text;
-    await tester.pumpWidget(widget((context) {
-      buildCount++;
-      text = $var(() => 1);
-
-      $fork(() {
-        return () => endCount++;
-      });
-      return Text(text.value.toString(), textDirection: TextDirection.ltr);
-    }, Key('')));
-    expect(find.text('1'), findsOneWidget);
-    text.value++;
-    await tester.pump();
-    expect(find.text('2'), findsOneWidget);
-    text.value++;
-    await tester.pump();
-    expect(find.text('3'), findsOneWidget);
-    expect(buildCount, 3);
-    expect(endCount, 2);
-    await tester.pumpWidget(Container());
-    expect(endCount, 3);
-  });
-
-  testWidgets('should not interfere with inner components', (tester) async {
-    $Var<bool> buildInner;
-    await tester.pumpWidget(widget((context) {
-      buildInner = $var(() => true);
-      if (buildInner.value) {
-        return widget((_) {
-          final cursor = $var(() => 'inner');
-          return Text(cursor.value, textDirection: TextDirection.ltr);
-        }, Key('inner'));
-      } else {
-        return widget((_) {
-          $var(() => 1);
-          final cursor = $var(() => 'outer');
-          return Text(cursor.value, textDirection: TextDirection.ltr);
-        }, Key('outer'));
-      }
-    }, Key('')));
-    expect(find.text('inner'), findsOneWidget);
-    buildInner.value = false;
-    await tester.pump();
-    expect(find.text('outer'), findsOneWidget);
-    buildInner.value = true;
-    await tester.pump();
-    expect(find.text('inner'), findsOneWidget);
-  });
 }
